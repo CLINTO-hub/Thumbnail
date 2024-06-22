ba7c8c1ec6da90627a95eb0b4a077f40a2d16d2c
CLINTO-hub <clintogeorge007@gmail.com>
2024-06-22 12:36:25 +0530

    token

--- Backend/middileware/ValidateApiKey.js
+++ Backend/middileware/ValidateApiKey.js
@@ -7,13 +7,11 @@ export const ValidateApiKey = async (req, res, next) => {
       return res.status(404).json({ message: 'User not found' });
     }
     console.log('key',user.apiKey);
-
     const apiKey = req.headers['x-api-key'];
     console.log('Head',apiKey);
     if (!apiKey || apiKey !== user.apiKey) {
       return res.status(401).json({ message: 'Invalid API Key' });
     }
-
     next();
   } catch (error) {
     console.log(error.message);
